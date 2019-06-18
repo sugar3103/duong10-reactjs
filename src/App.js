83686c6d0e024c160c390bfc97a121ad35264a25
Sugar <43548780+sugar3103@users.noreply.github.com>
2019-06-18 13:55:01 +0700

    add full project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header"
+import ProductList from "./components/ProductList"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <body>
+        {/* <!-- header start --> */}
+        <Header />
+        {/* <!-- header end --> */}
+
+        {/* <!-- shop-area start --> */}
+        <ProductList />
+        {/* <!-- shop-area end --> */}
+
+        {/* <!-- footer start --> */}
+        <Footer />
+        {/* <!-- footer end --> */}
+
+        {/* <!-- Fullscreen search --> */}
+        <div class="search-wrap">
+          <div class="search-inner">
+            <i class="fas fa-times search-close" id="search-close"></i>
+            <div class="search-cell">
+              <form method="get">
+                <div class="search-field-holder">
+                  <input type="search" class="main-search-input" placeholder="Search Entire Store..." />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        {/* <!-- end fullscreen search --> */}
+      </body>
+    </>
   );
 }
 
